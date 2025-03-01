096771975c8bee20ef6ee13667aaef47d3ddfb42
Vladislav Andonov <wladoandonov@gmail.com>
2025-03-01 23:15:29 +0200

    Twttr problem solved

---
+++ loops/twttr.py
@@ -0,0 +1,17 @@
+def main():
+    user_input = input("Input: ").strip()
+
+    print(shorten(user_input))
+
+
+def shorten(text):
+    shorten_word = ""
+
+    for l in text:
+        if l.lower() != "a" and l.lower() != "e" and l.lower() != "i" and l.lower() != "o" and l.lower() != "u":
+            shorten_word += l
+
+    return shorten_word
+
+
+main()
