ccb586ecc6ac5dce87f865519777fe517c9cc6c8
Vladislav Andonov <wladoandonov@gmail.com>
2025-03-02 17:50:11 +0200

    Vanity plates problem solved

---
+++ loops/plates.py
@@ -0,0 +1,39 @@
+def main():
+    plate = input("Plate: ")
+
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    
+    if not s[0:2].isalpha():
+        return False
+    
+    if 2 > len(s) or len(s) > 6:
+        return False
+
+    for i in range(len(s)):
+        
+        if not s[i].isnumeric() and not s[i].isalpha():
+            return False
+
+        first_digit_index = -1
+        for i, char in enumerate(s):
+            if char.isdigit():
+                first_digit_index = i
+                break
+
+        if first_digit_index != -1:
+            if any(char.isalpha() for char in s[first_digit_index:]):
+                return False
+
+        if s[first_digit_index] == "0":
+            return False
+
+    return True
+
+
+main()
