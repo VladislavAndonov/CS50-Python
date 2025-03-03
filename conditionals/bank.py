bd407371c3ed53f5b706bc269f0e3a26b9830cca
Vladislav Andonov <wladoandonov@gmail.com>
2025-03-03 22:28:20 +0200

    Bank problem solved

---
+++ conditionals/bank.py
@@ -0,0 +1,19 @@
+def main():
+
+    greeting = input("Greeting: ").strip()
+    amount = value(greeting)
+
+    print(f"${amount}")
+
+
+def value(greeting):
+    greeting = greeting.lower()
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
+    else:
+        return 100
+
+
+main()
