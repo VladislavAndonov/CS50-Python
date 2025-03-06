90ea668ca4abcb9aadfdce81ce4a72eeb498d9b3
Vladislav Andonov <wladoandonov@gmail.com>
2025-03-06 22:27:18 +0200

    Fuel Gauge problem solved

---
+++ exceptions/fuel.py
@@ -0,0 +1,41 @@
+def main():
+    while True:
+        fraction = input("Fraction: ")
+        percentage = convert(fraction)
+
+        if isinstance(percentage, int):
+            print(gauge(percentage))
+            break
+
+
+def convert(fraction):
+    try:
+        x, y = fraction.split("/")
+
+        x = int(x)
+        y = int(y)
+
+        if y == 0:
+            raise ZeroDivisionError
+
+        if x > y:
+            raise ValueError
+
+    except ValueError:
+        return ValueError
+    except ZeroDivisionError:
+        return ZeroDivisionError
+
+    return round(x / y * 100)
+
+
+def gauge(percentage):
+    if percentage >= 99:
+        return "F"
+    elif percentage <= 1:
+        return "E"
+    else:
+        return f"{percentage}%"
+
+
+main()
